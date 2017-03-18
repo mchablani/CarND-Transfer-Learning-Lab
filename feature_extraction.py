be87fefc9e5ff49b2d2fc4ef6094d5b0a2aedf01
Manish Chablani <chablani@uber.com>
2017-03-17 23:05:37 -0700

    init

---
+++ feature_extraction.py
@@ -0,0 +1,57 @@
+import pickle
+import tensorflow as tf
+import numpy as np
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+# command line flags
+flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
+flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
+
+flags.DEFINE_string('batch_size', 256, "batch_size")
+flags.DEFINE_string('epochs', 5, "epochs")
+
+
+def load_bottleneck_data(training_file, validation_file):
+    """
+    Utility function to load bottleneck features.
+
+    Arguments:
+        training_file - String
+        validation_file - String
+    """
+    print("Training file", training_file)
+    print("Validation file", validation_file)
+
+    with open(training_file, 'rb') as f:
+        train_data = pickle.load(f)
+    with open(validation_file, 'rb') as f:
+        validation_data = pickle.load(f)
+
+    X_train = train_data['features']
+    y_train = train_data['labels']
+    X_val = validation_data['features']
+    y_val = validation_data['labels']
+
+    return X_train, y_train, X_val, y_val
+
+
+def main(_):
+    # load bottleneck data
+    X_train, y_train, X_val, y_val = load_bottleneck_data(FLAGS.training_file, FLAGS.validation_file)
+
+    print(X_train.shape, y_train.shape)
+    print(X_val.shape, y_val.shape)
+
+    nb_classes = len(np.unique(y_train))
+
+
+    # TODO: train your model here
+
+
+# parses flags and calls the `main` function above
+if __name__ == '__main__':
+    tf.app.run()
